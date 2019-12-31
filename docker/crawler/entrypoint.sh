bf43abd42f13adb1bbbd365df1336b251f4775b4
Iván José Martín García <ivanmartinga@gmail.com>
2019-12-31 13:50:00 +0100

    Changes in docker set up

--- docker/crawler/entrypoint.sh
+++ docker/crawler/entrypoint.sh
@@ -7,6 +7,7 @@ if [ -z "${POSTGRES_USER}" ]; then
     base_postgres_image_default_user='postgres'
     export POSTGRES_USER="${base_postgres_image_default_user}"
 fi
+
 export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
 
 postgres_ready() {
@@ -26,6 +27,7 @@ except psycopg2.OperationalError:
 sys.exit(0)
 END
 }
+
 until postgres_ready; do
   >&2 echo 'Waiting for PostgreSQL to become available...'
   sleep 1
