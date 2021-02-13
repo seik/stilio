973eddf261fe97da1340ce72715a75a03c04e47b
seik <ivanmartinga@gmail.com>
2021-02-13 02:23:16 +0100

    Store files in torrent model

--- stilio/persistence/torrents/models.py
+++ stilio/persistence/torrents/models.py
@@ -7,8 +7,8 @@ from peewee import (
     BigIntegerField,
     CharField,
     DateTimeField,
-    ForeignKeyField,
     IntegerField,
+    TextField,
 )
 from playhouse.postgres_ext import Match, TSVectorField
 
@@ -17,21 +17,18 @@ from stilio.persistence.database import BaseModel
 
 class Torrent(BaseModel):
     info_hash = CharField(max_length=40, unique=True)
+
     name = CharField(max_length=512)
     search_name = TSVectorField()
 
     seeders = IntegerField(default=0)
     leechers = IntegerField(default=0)
 
-    last_update = DateTimeField(default=dt.datetime.now)
-    last_update_change = DateTimeField(default=dt.datetime.now)
+    files = TextField()
+
+    size = BigIntegerField()
 
     added_at = DateTimeField(default=dt.datetime.now)
-
-    @property
-    def size(self) -> int:
-        # TODO handle this with an aggregation
-        return sum([file.size for file in self.files])
 
     def __str__(self):
         return self.name
@@ -65,13 +62,3 @@ class Torrent(BaseModel):
         )
 
         return torrents, torrent_count
-
-
-class File(BaseModel):
-    torrent = ForeignKeyField(Torrent, backref="files")
-
-    path = CharField(max_length=512)
-    size = BigIntegerField()
-
-    def __str__(self):
-        return self.path
--- stilio/persistence/utils.py
+++ stilio/persistence/utils.py
@@ -1,35 +1,92 @@
-from typing import List
+import json
 
 from peewee import IntegrityError
 from playhouse.postgres_ext import fn
 
 from stilio.persistence.exceptions import StoringError
-from stilio.persistence.torrents.models import File, Torrent
+from stilio.persistence.torrents.models import Torrent
+
+
+class FileSystem:
+    def __init__(self, file_path=None, root=False):
+        self.children = []
+
+        if file_path != None and not root:
+            try:
+                self.name, child = file_path.split("/", 1)
+                self.children.append(FileSystem(child))
+            except ValueError:
+                self.name = file_path
+        else:
+            self.name = "/"
+
+    def add_child(self, file_path):
+        try:
+            first_level, next_level = file_path.split("/", 1)
+        except ValueError:
+            # only one result, final file in path, add it
+            self.children.append(FileSystem(file_path))
+        else:
+            # search for child and add the rest
+            for child in self.children:
+                if first_level == child.name:
+                    child.add_child(next_level)
+                    return
+            else:
+                # rest of the path not present in filesystem, add it
+                self.children.append(FileSystem(file_path))
+
+    def is_children(self, name: str) -> bool:
+        return any(name == children.name for children in self.children)
+
+    def make_dict(self):
+        if len(self.children) > 0:
+            dictionary = {self.name: []}
+            for child in self.children:
+                dictionary[self.name].append(child.make_dict())
+            return dictionary
+        else:
+            return self.name
 
 
 def store_metadata(info_hash: bytes, metadata: dict, logger=None) -> None:
     name = metadata[b"name"].decode("utf-8")
+    files = get_file_structure(metadata, name)
+    size = get_size(metadata)
 
-    files: List[dict] = []
+    try:
+        Torrent.insert(
+            info_hash=info_hash.hex(),
+            name=name,
+            search_name=fn.to_tsvector(name),
+            files=json.dumps(files),
+            size=size,
+        ).execute()
+    except IntegrityError as e:
+        logger.exception(e)
+        return
+
+    if logger:
+        logger.info(f"Added: {name}")
+
+
+def get_file_structure(metadata: dict, name: str) -> dict:
+    files = FileSystem(root=True)
+
     if b"files" in metadata:
         for file in metadata[b"files"]:
             if any(b"/" in item for item in file[b"path"]):
                 raise StoringError(message="Path contains trailing slashes")
             path = "/".join(i.decode("utf-8") for i in file[b"path"])
-            files.append({"path": path, "size": file[b"length"]})
+            files.add_child(path)
     else:
-        files.append({"path": name, "size": metadata[b"length"]})
+        files.add_child(name)
 
-    try:
-        torrent = Torrent.insert(
-            info_hash=info_hash.hex(), name=name, search_name=fn.to_tsvector(name)
-        ).execute()
-    except IntegrityError as e:
-        logger.exception(e)
-        return
+    return files.make_dict()
+
+
+def get_size(metadata: dict) -> int:
+    if b"files" in metadata:
+        return sum([element[b"length"] for element in metadata[b"files"]])
     else:
-        for file in files:
-            File.insert(path=file["path"], size=file["size"], torrent=torrent).execute()
-
-    if logger:
-        logger.info(f"Added: {name}")
+        return 0
